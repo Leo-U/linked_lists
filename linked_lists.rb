adfe676ce99d0cc16dd842dab9fe9539c01e42a9
Joel Howard <joelhoward2@gmail.com>
2023-02-05 17:49:59 -0500

    add initialize methods, add append and prepend methods

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,17 +1,32 @@
 class LinkedList
+  attr_reader :list
+  def initialize
+    @list = []
+    @head = nil
+  end
+
   def append(value)
+    node = Node.new(value)
+    last_node = @list[-1]
+    last_node.next_node = node if !@list.empty?
+    @list << node
   end
 
   def prepend(value)
+    node = Node.new(value)
+    @list = [node] + @list
+    node.next_node = @list[1]
   end
 
   def size
   end
 
   def head
+    @list[0]
   end
 
   def tail
+    @list[-1]
   end
 
   def at(index)
@@ -32,9 +47,20 @@ class LinkedList
 end
 
 class Node
-  def value
-  end
+  attr_accessor :value, :next_node
 
-  def next_node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
+end
+
+newList = LinkedList.new()
+newList.append(3)
+newList.append('q')
+newList.prepend(33)
+
+newList.list.each do |el|
+  p el
+  p ''
 end
